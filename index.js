cc2b4093f099e9827281204f543477fd7e3fac3b
Esad <69761769+Esad27@users.noreply.github.com>
2022-06-27 01:36:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+/*
+const port = process.env.PORT || port;
+const www = process.env.WWW || './';
+app.use(express.static(www));
+console.log(`serving ${www}`);*/
+app.use(express.json());
+
+const datalist = [];
+
+
+app.get('/data', (req, res) => {
+    //res.sendFile(`index.html`, { root: www });
+    res.status(200).send(datalist);
+
+});
+app.post('/data', (req, res) => {
+    let data = req.body;
+    datalist.push(data);
+    res.status(201).send(data);
+    return;
+    //curl -X POST http://localhost:3000/data -H "Content-Type: application/json" -d "{\"sa\":\"As\"}"
+
+});
+app.listen(3000, () => {
+
+    console.log("server is running");
+
+
+});
