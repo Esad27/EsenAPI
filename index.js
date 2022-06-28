6ef9e34ebc07a306b3a10c32face3df2e27803dc
Esad <69761769+Esad27@users.noreply.github.com>
2022-06-28 22:36:26 +0300

    deneme

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.use(express.json());
 
 const datalist = [];
 
+ var saniye = 0, dakika = 0, saat = 0;
+  function bak()
+  {
+    if(saniye < 59) saniye = saniye + 1;
+    else
+    {
+      saniye = 0; 
+      if(dakika < 59) dakika = dakika + 1;
+      else{dakika = 0; saat = saat + 1;}
+    }
+    $("#sure").html(saat + " : " + dakika + " : " + saniye);
+  }
+  $(document).ready(function(){
+    $("#sure").html("0 : 0 : 0");
+    setInterval(bak, 1000);
+  });
 
 app.get('/data', (req, res) => {
     //res.sendFile(`index.html`, { root: www });
