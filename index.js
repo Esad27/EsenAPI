1632cbcad502eaf23bf099f4dde88e6288ef8755
Esad <esadbjk@gmail.com>
2022-07-24 20:19:14 +0300

    git push

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
-const app = express();
+
 const port = process.env.PORT || 3000;
+const firestore = require('./FirebaseConfig')
+const FirebaseService = require('./src/FirebaseService')
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit')
+
+
+const app = express();
+//limit
+app.use(helmet());
+app.use(compression());
+app.use(limiter);
+
+//limeter
+
+
+
+
+
+
 /*
 const port = process.env.PORT || port;
 const www = process.env.WWW || './';
@@ -11,13 +31,21 @@ app.use(express.json());
 const datalist = [];
 
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
- 
-app.get('/data', (req, res) => {
+
+
+app.get('/data/:sa', (req, res) => {
     //res.sendFile(`index.html`, { root: www });
-    res.status(200).send(datalist);
+    res.status(200).send(req.params.sa);
 
 });
+
 app.post('/data', (req, res) => {
     let data = req.body;
     datalist.push(data);
@@ -32,3 +60,4 @@ app.listen(port, () => {
 
 
 });
+/* ayrı yerler yapıcaksın tabi önce hesap kitao yapıcan ona göre hareket edicen ;)  sekmeler sana yardımcı olur haberin olsun tmm dırr kapa :)*/
